38461d0bed37caddd6936b7b4c0da46f8fa4cb10
Luiz Felipe Neves <luizfelipe2310@gmail.com>
2020-06-22 03:12:13 -0300

    Added validation to simulation route

--- src/API/routes/simulacao.route.js
+++ src/API/routes/simulacao.route.js
@@ -1,10 +1,21 @@
+import Joi from '@hapi/joi';
 import express from 'express';
+import { createValidator } from 'express-joi-validation';
 
 import { getSimulacao } from '../controllers/Simulacao';
 
+const validator = createValidator();
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const querySchema = Joi.object({
+  plano: Joi.string().required(),
+  origem: Joi.string().required(),
+  destino: Joi.string().required(),
+  minutos: Joi.string().required(),
+});
+
+router.get('/', validator.query(querySchema), (req, res) => {
   const data = getSimulacao(req.query);
   return res.json(data);
 });
